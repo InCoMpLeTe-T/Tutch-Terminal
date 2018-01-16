0adfa1a2a62461bd0824c9f5b01ae8fcb86f078a
PLATO\mlch.skp <mlch.skp@edu.mercantec.dk>
2018-01-16 10:40:54 +0100

    her er første upload

---
+++ Fredagscafe/Page2.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fredagscafe
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Page1 p1 = new Page1();
+            this.NavigationService.Navigate(p1);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Page3 p3 = new Page3();
+            this.NavigationService.Navigate(p3);
+        }
+    }
+}
