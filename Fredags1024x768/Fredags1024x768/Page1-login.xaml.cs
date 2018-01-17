fccdcfc2b97e5b38ba30a6dd687fbb634cee13aa
PLATO\mlch.skp <mlch.skp@edu.mercantec.dk>
2018-01-17 09:37:26 +0100

    here is main terminal done

---
+++ Fredags1024x768/Fredags1024x768/Page1-login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fredags1024x768
+{
+    /// <summary>
+    /// Interaction logic for Page1_login.xaml
+    /// </summary>
+    public partial class Page1_login : Page
+    {
+        public Page1_login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Page1_MainMeny p2 = new Page1_MainMeny();
+            this.NavigationService.Navigate(p2);
+        }
+    }
+}
---
+++ Fredags1024x768/Fredags1024x768/Page2-MainMeny.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fredags1024x768
+{
+    /// <summary>
+    /// Interaction logic for Page1_MainMeny.xaml
+    /// </summary>
+    public partial class Page1_MainMeny : Page
+    {
+        public Page1_MainMeny()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Page1_login p1 = new Page1_login();
+            this.NavigationService.Navigate(p1);
+        }
+    }
+}
